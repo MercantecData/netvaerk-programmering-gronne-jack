73fb6aed06ffe7bccae232977a8545c39e7bb2dc
Gronne <kasperg102@gmail.com>
2019-12-09 09:00:03 +0100

    Opgave 1

---
+++ Netværk Programmering/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+namespace Netværk_Programmering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text1 = "ÆØÅLOLMAOÆØÅ";
+            var bytes1 = Encoding.ASCII.GetBytes(text1);
+
+            Console.WriteLine("Done!");
+
+            text1 = Encoding.ASCII.GetString(bytes1);
+
+            string text2 = "Hello World! ÆØÅ ÆØÅ LÆØÅ YE";
+            var bytes = Encoding.UTF8.GetBytes(text2);
+
+            Console.WriteLine("Done!");
+
+            text2 = Encoding.UTF8.GetString(bytes);
+
+            Console.WriteLine(text2);
+            
+            
+        }
+    }
+}
